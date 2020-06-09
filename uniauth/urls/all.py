ed584ed690bda67ee06b6c1df2b5f56c16cfa944
Lance Goodridge <ldgoodridge95@gmail.com>
2020-06-08 21:10:28 -0400

    Updated verify-token URL regex to handle new token length

--- uniauth/urls/all.py
+++ uniauth/urls/all.py
@@ -11,9 +11,9 @@ urlpatterns = [
     url(r'^settings/$', views.settings, name='settings'),
     url(r'^link-to-profile/$', views.link_to_profile, name='link-to-profile'),
     url(r'^link-from-profile/(?P<institution>[a-z0-9\-]+)/$', views.link_from_profile, name='link-from-profile'),
-    url(r'^verify-token/(?P<pk_base64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.verify_token, name='verify-token'),
+    url(r'^verify-token/(?P<pk_base64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$', views.verify_token, name='verify-token'),
     url(r'^password-reset/', views.PasswordReset.as_view(), name='password-reset'),
     url(r'^password-reset-done/', views.PasswordResetDone.as_view(), name='password-reset-done'),
-    url(r'^password-reset-verify/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.PasswordResetVerify.as_view(), name='password-reset-verify'),
+    url(r'^password-reset-verify/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/', views.PasswordResetVerify.as_view(), name='password-reset-verify'),
     url(r'^password-reset-verify-done/', views.PasswordResetVerifyDone.as_view(), name='password-reset-verify-done'),
 ]
